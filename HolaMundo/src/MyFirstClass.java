739ed939aa66c1bf433604efdb68b7fd2e9bd24a
rodri <rodri@rodri-PC>
2018-10-04 23:22:10 +0200

    Original

---
+++ HolaMundo/src/MyFirstClass.java
@@ -0,0 +1,11 @@
+/*
+ * APELLIDOS, Nombre: DIAZ, Rodrigo
+ * Fecha: 04/10/2018
+ * Módulo: Programación de servicios y procesos
+ * Práctica Nº1 Instalación y manejo de git/github
+ * */
+public class MyFirstClass {
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+	}
+}
